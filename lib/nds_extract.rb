efb051fc46864a4bd0c30e919fac2d161f418782
Luke Lin <lukelin1991@yahoo.com>
2019-12-07 16:54:25 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -65,11 +65,8 @@ def gross_per_studio(collection)
   a_gross = 0
   f_gross = 0
   while idx < collection.length do
-    movie = collection[idx]
     binding.pry
-    temp_arr = []
-
-
+    movie = collection[idx]
     if collection[idx][:studio] == "Alpha Films"
       a_gross += collection[idx][:worldwide_gross]
     else
