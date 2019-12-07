4d4dccf3ce1e286fb13bbfc852ba740fb4a5e7c6
Luke Lin <lukelin1991@yahoo.com>
2019-12-07 17:48:19 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -70,7 +70,6 @@ def gross_per_studio(collection)
     end
     idx += 1
   end
-
   return new_hash
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
   # return a Hash that includes the total worldwide_gross of all the movies from
